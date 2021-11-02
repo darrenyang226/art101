1f527ce11a488272f5e9a407543c6db1fce90e4e
darrenyang226 <74940508+darrenyang226@users.noreply.github.com>
2021-11-01 23:05:37 -0700

    lab 9

---
+++ lab9/js/lab.js
@@ -0,0 +1,20 @@
+// Lab 9 JavaScript for the Web
+// A Javascript file with commands to function for the Web.
+// @author Ryan McCarty and Darren Yang
+// @link: https://darrenyang226.github.io/art101/lab9/
+// @since  11.01.2021
+
+ var outputEl = document.getElementById("output");
+ var new1El =  document.createElement("div");
+ new1El.id = "button";
+ new1El.innerHTML = "Press me";
+ var output2El = document.getElementById("output2");
+ var new2El = document.createElement("div");
+ new2El.id = "button2";
+ new2El.innerHTML = "Don't Press me ";
+ output.appendChild(new1El);
+ output2.appendChild(new2El);
+ document.getElementById("id1").style.color = "red";
+ document.getElementById("id2").style.color = "blue";
+ document.getElementById("id3").style.color = "blue";
+ document.getElementById("id4").style.color = "blue";
